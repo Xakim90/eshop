d2195a65dcf62aeb110d2fc56bd1f61aa1f86e24
Xakimjon <xakim90@gmail.com>
2021-03-23 11:31:41 +0500

    fixed phones page

--- resources/js/react/js/Layout/Content/ContentMain.js
+++ resources/js/react/js/Layout/Content/ContentMain.js
@@ -17,64 +17,67 @@ import Test from "../../Pages/Test/Test";
 const ContentMain = (props) => {
     return (
         <div className="mt-1 md:mt-5">
-                    <Switch>
-                        <Route exact path="/">
-                            <Home
-                                productsIsLoaded={props.data.productsIsLoaded}
-                                products={props.data.products}
-                            />
-                        </Route>
-                        <Route exact path="/phones">
-                            <Phones brands={props.data.brands} products={props.data.products} categories={props.data.categories} />
-                        </Route>
-                        <Route exact path="/brands">
-                            <Brands brands={props.data.brands} />
-                        </Route>
-                        <Route exact path="/notebooks">
-                            <Notebooks />
-                        </Route>
-                        <Route exact path="/tv">
-                            <Tv />
-                        </Route>
-                        <Route exact path="/appliances">
-                            <Appliances />
-                        </Route>
-                        <Route exact path="/sports">
-                            <Sports />
-                        </Route>
-                        <Route exact path="/register">
-                            <Register />
-                        </Route>
-                        <Route path="/addproduct">
-                            <AddProduct
-                                createProduct={props.data.createProduct}
-                                brands={props.data.brands}
-                                categories={props.data.categories}
-                            />
-                        </Route>
-                        <Route path="/addcategory">
-                            <AddCategory
-                                createCategory={props.data.createCategory}
-                                catalogs={props.data.catalogs}
-                            />
-                        </Route>
-                        <Route path="/addcatalog">
-                            <AddCatalog
-                                createCatalog={props.data.createCatalog}
-                            />
-                        </Route>
-                        <Route path="/addbrand">
-                            <AddBrand
-                                createBrand={props.data.createBrand}
-                                categories={props.data.categories}
-                            />
-                        </Route>
-                        <Route path="/test">
-                            <Test />
-                        </Route>
-                    </Switch>
-                </div>
-       );
+            <Switch>
+                <Route exact path="/">
+                    <Home
+                        productsIsLoaded={props.data.productsIsLoaded}
+                        products={props.data.products}
+                    />
+                </Route>
+                <Route exact path="/phones">
+                    <Phones
+                        brands={props.data.brands}
+                        products={props.data.products}
+                        categories={props.data.categories}
+                        productsIsLoaded={props.data.productsIsLoaded}
+                    />
+                </Route>
+                <Route exact path="/brands">
+                    <Brands brands={props.data.brands} />
+                </Route>
+                <Route exact path="/notebooks">
+                    <Notebooks />
+                </Route>
+                <Route exact path="/tv">
+                    <Tv />
+                </Route>
+                <Route exact path="/appliances">
+                    <Appliances />
+                </Route>
+                <Route exact path="/sports">
+                    <Sports />
+                </Route>
+                <Route exact path="/register">
+                    <Register />
+                </Route>
+                <Route path="/addproduct">
+                    <AddProduct
+                        createProduct={props.data.createProduct}
+                        brands={props.data.brands}
+                        categories={props.data.categories}
+                    />
+                </Route>
+                <Route path="/addcategory">
+                    <AddCategory
+                        createCategory={props.data.createCategory}
+                        catalogs={props.data.catalogs}
+                    />
+                </Route>
+                <Route path="/addcatalog">
+                    <AddCatalog createCatalog={props.data.createCatalog} />
+                </Route>
+                <Route path="/addbrand">
+                    <AddBrand
+                        createBrand={props.data.createBrand}
+                        categories={props.data.categories}
+                    />
+                </Route>
+                <Route path="/test">
+                    <Test />
+                </Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default ContentMain;
--- resources/js/react/js/Pages/Phones.js
+++ resources/js/react/js/Pages/Phones.js
@@ -1,27 +1,52 @@
 import React, { useState } from "react";
+import RecipeReviewCard from "../components/RecipeReviewCard";
 
-const Phones = (props) => {
-    const [state, setState] = useState({
-        
-    });
-    // const open = () => {
-
-    // }
+const Phones = props => {
+    const [brand, setBrand] = useState({ currentBrand: "" });
     return (
-        <div className="mt-5 w-full h-full flex justify-around">
-            {props.categories.map(category =>
-                category.catalog_id === 1 ? (
-                    <div className="text-gray-500" key={category.id}>
-                        <h3>{category.title}</h3>
-                        {props.brands.map(brand =>
-                            brand.category_id === category.id ? (
-                                <a className="block" href={brand.name} key={brand.id}>{brand.name}</a>
-                            ) : null
-                        )}
-                    </div>
-                ) : null
-            )}
-        </div>
+        <>
+            <div className="mt-5 w-full h-full flex justify-around">
+                {props.categories.map(category =>
+                    category.catalog_id === 1 ? (
+                        <div
+                            className="text-gray-500 text-2xl"
+                            key={category.id}
+                        >
+                            <p>{category.title}</p>
+                            {props.brands.map(brand =>
+                                brand.category_id === category.id ? (
+                                    <a
+                                        onClick={() =>
+                                            setBrand({
+                                                currentBrand: brand.name
+                                            })
+                                        }
+                                        className="block cursor-pointer text-lg"
+                                        key={brand.id}
+                                    >
+                                        {brand.name}
+                                    </a>
+                                ) : null
+                            )}
+                        </div>
+                    ) : null
+                )}
+            </div>
+            <div className="grid grid-cols-6 gap-4">
+                {brand.currentBrand !== ""
+                    ? props.products.map((product, index) =>
+                          brand.currentBrand === product.brandName ? (
+                              <div className="text-red-500 " key={index}>
+                                  <RecipeReviewCard
+                                      productsIsLoaded={props.productsIsLoaded}
+                                      data={product}
+                                  />
+                              </div>
+                          ) : null
+                      )
+                    : null}
+            </div>
+        </>
     );
 };
 
